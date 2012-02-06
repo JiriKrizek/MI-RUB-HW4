c1a25a1f6469b18cc60fe09eceb31073e339da80
Jiri Krizek <jkrizek@gmail.com>
2012-02-06 13:52:36 +0100

    tabs to spaces

--- bin/decipher.rb
+++ bin/decipher.rb
@@ -1,41 +1,41 @@
 
 
 class Decipher
-	MIN_CHAR_VAL=32
-	MAX_CHAR_VAL=126
+  MIN_CHAR_VAL=32
+  MAX_CHAR_VAL=126
 
-	def decipher(text)
-		unless text.respond_to?(:to_s)
-			fail TypeError.new("'text' needs 'to_s' capability")
-		end
-		result = ""
-		text.to_s.each_char  do |c|
-			result << transform(c, -7)
-		end
-		result
-	end
+  def decipher(text)
+    unless text.respond_to?(:to_s)
+      fail TypeError.new("'text' needs 'to_s' capability")
+    end
+    result = ""
+    text.to_s.each_char  do |c|
+      result << transform(c, -7)
+    end
+    result
+  end
 
-	def cipher(text)
-		unless text.respond_to?(:to_s)
-			fail TypeError.new("'text' needs 'to_s' capability")
-		end
+  def cipher(text)
+    unless text.respond_to?(:to_s)
+      fail TypeError.new("'text' needs 'to_s' capability")
+    end
 
-		result = ""
+    result = ""
 
-		text.to_s.each_char  do |c|
-			result << transform(c, +7)
-		end
-		result
-	end
+    text.to_s.each_char  do |c|
+      result << transform(c, +7)
+    end
+    result
+  end
 
-	def transform(char, shift)
-		ch = char.ord+shift
-		if(ch >= MIN_CHAR_VAL && ch <= MAX_CHAR_VAL)
-			ch.chr
-		else
-			ch
-		end
-	end
+  def transform(char, shift)
+    ch = char.ord+shift
+    if(ch >= MIN_CHAR_VAL && ch <= MAX_CHAR_VAL)
+      ch.chr
+    else
+      ch
+    end
+  end
 end
 
 d = Decipher.new
